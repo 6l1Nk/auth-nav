fe43d4a5f44efc8462497d426723240a2d572bd4
6l1Nk <nope@who.wat>
2024-04-25 01:32:22 -0500

    sign up form

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/6l1Nk/auth-nav
+module github.com/6liNk/auth-nav
 
 go 1.22.2
--- main.go
+++ main.go
@@ -12,18 +12,28 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func loginHandler(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte(`<button id="logout" hx-get="/logout" hx-trigger="click" hx-swap="innerHTML" hx-target="#nav-login" hx-indicator=".loading">Logout</button>`))
+    // w.Write([]byte(`<button id="logout" hx-get="/logout" hx-trigger="click" hx-swap="innerHTML" hx-target="#nav-login" hx-indicator=".loading">Logout</button>`))
+    button := template.Must(template.ParseFiles("templates/logout-button.html"))
+    button.Execute(w, nil)
 }
 
 func logoutHandler(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte(`<button id="login" hx-get="/login" hx-trigger="click" hx-swap="innerHTML" hx-target="#nav-login" hx-indicator=".loading">Login</button>`))
-    w.Write([]byte(`<button id="signup">Sign Up</button>`))
+    // w.Write([]byte(`<button id="login" hx-get="/login" hx-trigger="click" hx-swap="innerHTML" hx-target="#nav-login" hx-indicator=".loading">Login</button>`))
+    // w.Write([]byte(`<button id="signup">Sign Up</button>`))
+    loginSignupButtons := template.Must(template.ParseFiles("templates/login-signup-buttons.html"))
+    loginSignupButtons.Execute(w, nil)
+}
+
+func signUpHandler(w http.ResponseWriter, r *http.Request) {
+    signUpForm := template.Must(template.ParseFiles("templates/signup.html"))
+    signUpForm.Execute(w, nil)
 }
 
 func main() {
     http.HandleFunc("/", indexHandler)
     http.HandleFunc("/login", loginHandler)
     http.HandleFunc("/logout", logoutHandler)
+    http.HandleFunc("/signup-form", signUpHandler)
     http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
     fmt.Println("Server is running on http://localhost:8080")
     http.ListenAndServe(":8080", nil)
