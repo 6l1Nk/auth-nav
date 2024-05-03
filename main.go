118be117644acb933adedde55cbaaefc1cdd4501
6l1Nk <nope@who.wat>
2024-05-03 16:28:06 -0500

    init database

--- main.go
+++ main.go
@@ -14,10 +14,10 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
     tmpl.Execute(w, nil)
 }
 
-func signInHandler(w http.ResponseWriter, r *http.Request) {
-    tmpl := template.Must(template.ParseFiles("templates/signin.html"))
-    tmpl.Execute(w, nil)
-}
+// func signInHandler(w http.ResponseWriter, r *http.Request) {
+//     tmpl := template.Must(template.ParseFiles("templates/signin.html"))
+//     tmpl.Execute(w, nil)
+// }
 
 func signUpHandler(w http.ResponseWriter, r *http.Request) {
     if r.Method == http.MethodGet {
@@ -25,57 +25,57 @@ func signUpHandler(w http.ResponseWriter, r *http.Request) {
         tmpl.Execute(w, nil)
     }
 
-    if r.Method == http.MethodPost {
-        fmt.Println("post")
-        err := r.ParseForm()
-        if err != nil {
-            http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-            return
-        }
-
-        fmt.Println("canary")
-        // Parse form data
-        email := r.Form.Get("email")
-        password := r.Form.Get("password")
-        confirmPassword := r.Form.Get("confirm-password")
-
-        fmt.Println(email)
-        fmt.Println(password)
-        fmt.Println(confirmPassword)
-        // // Check if passwords match
-        // if password != confirmPassword {
-        //     http.Error(w, "Passwords do not match", http.StatusBadRequest)
-        //     return
-        // }
-        //
-        // Check if email already exists in the database
-        // var count int
-        // err = db.QueryRow("SELECT COUNT(*) FROM users WHERE email = ?", email).Scan(&count)
-        // if err != nil {
-        //     http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-        //     return
-        // }
-        // if count > 0 {
-        //     http.Error(w, "Email already exists", http.StatusConflict)
-        //     return
-        // }
-        //
-        // // Insert new user record into the database
-        // _, err = db.Exec("INSERT INTO users (email, password) VALUES (?, ?)", email, password)
-        // if err != nil {
-        //     http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-        //     return
-        // }
-        //
-        // // Success response
-        // w.WriteHeader(http.StatusCreated)
-        // fmt.Fprintf(w, "Account created successfully!")
-        tmpl := template.Must(template.ParseFiles("templates/index.html"))
-        tmpl.Execute(w, nil)
-    }
+//     if r.Method == http.MethodPost {
+//         fmt.Println("post")
+//         err := r.ParseForm()
+//         if err != nil {
+//             http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+//             return
+//         }
+//
+//         fmt.Println("canary")
+//         // Parse form data
+//         email := r.Form.Get("email")
+//         password := r.Form.Get("password")
+//         confirmPassword := r.Form.Get("confirm-password")
+//
+//         fmt.Println(email)
+//         fmt.Println(password)
+//         fmt.Println(confirmPassword)
+//         // // Check if passwords match
+//         // if password != confirmPassword {
+//         //     http.Error(w, "Passwords do not match", http.StatusBadRequest)
+//         //     return
+//         // }
+//         //
+//         // Check if email already exists in the database
+//         // var count int
+//         // err = db.QueryRow("SELECT COUNT(*) FROM users WHERE email = ?", email).Scan(&count)
+//         // if err != nil {
+//         //     http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+//         //     return
+//         // }
+//         // if count > 0 {
+//         //     http.Error(w, "Email already exists", http.StatusConflict)
+//         //     return
+//         // }
+//         //
+//         // // Insert new user record into the database
+//         // _, err = db.Exec("INSERT INTO users (email, password) VALUES (?, ?)", email, password)
+//         // if err != nil {
+//         //     http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+//         //     return
+//         // }
+//         //
+//         // // Success response
+//         // w.WriteHeader(http.StatusCreated)
+//         // fmt.Fprintf(w, "Account created successfully!")
+//         tmpl := template.Must(template.ParseFiles("templates/index.html"))
+//         tmpl.Execute(w, nil)
+//     }
 }
 
-func main() {
+func initDatabase() {
     db, err := sql.Open("sqlite3", "users.db")
     if err != nil { 
         panic(err)
@@ -83,18 +83,23 @@ func main() {
     defer db.Close()
 
     _, err = db.Exec(`CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    email TEXT UNIQUE,
-    password TEXT
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        email TEXT UNIQUE,
+        password TEXT
     )`)
     if err != nil {
         panic(err)
     }
+}
+
+func main() {
+    initDatabase()
 
     http.HandleFunc("/", indexHandler)
-    http.HandleFunc("/signin", signInHandler)
-    http.HandleFunc("/signup", signUpHandler)
+    // http.HandleFunc("/signin", signInHandler)
+    // http.HandleFunc("/signup", signUpHandler)
     http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
     fmt.Println("Server is running on http://localhost:8080")
     http.ListenAndServe(":8080", nil)
 }
--- main_test.go
+++ main_test.go
@@ -5,6 +5,9 @@ import (
     "net/http/httptest"
     "testing"
     "strings"
+	"database/sql"
+
+    _ "github.com/mattn/go-sqlite3"
 )
 
 func TestIndexHandler(t *testing.T) {
@@ -28,12 +31,103 @@ func TestIndexHandler(t *testing.T) {
     if !strings.Contains(res.Body.String(),"<nav>") {
         t.Errorf("handler returned unexpected body: missing <nav>")
     }
-
-    if !strings.Contains(res.Body.String(),"<button id=\"login\">") {
-        t.Errorf("handler returned unexpected body: missing <button id=\"login\">")
+    
+    loginButtonHTML := "<button id=\"login\">"
+    if !strings.Contains(res.Body.String(), loginButtonHTML) {
+        t.Errorf("handler returned unexpected body: missing %v", loginButtonHTML)
     }
 
-    if !strings.Contains(res.Body.String(),"<button id=\"signup\">") {
-        t.Errorf("handler returned unexpected body: missing <button id=\"signup\">")
+    signUpButtonHTML := "<button id=\"signup\">"
+    if !strings.Contains(res.Body.String(), signUpButtonHTML) {
+        t.Errorf("handler returned unexpected body: missing %v", signUpButtonHTML)
     }
 }
+
+func TestSignUpGet(t *testing.T) {
+    req, err := http.NewRequest("GET", "/sign-up", nil)
+    if err != nil {
+        t.Fatal(err)
+    }
+
+    res := httptest.NewRecorder()
+    handler := http.HandlerFunc(signUpHandler)
+
+    handler.ServeHTTP(res, req)
+
+    if status := res.Code; status != http.StatusOK {
+        t.Errorf(
+            "handler returned wrong status code: got %v and expected %v",
+            status,
+            http.StatusOK)
+    }
+
+    signUpFormHTML := "<form action=\"/signup\" method=\"post\" class=\"signup-form\">" 
+
+    if !strings.Contains(res.Body.String(),signUpFormHTML) {
+        t.Errorf("handler returned unexpected body: missing %v",
+        signUpFormHTML)
+    }
+}
+
+
+func TestDatabaseSchema(t *testing.T) {
+    initDatabase()
+
+	db, err := sql.Open("sqlite3", "users.db")
+	if err != nil {
+		t.Fatalf("error opening datavase %v", err)
+	}
+	defer db.Close()
+    
+    rows, err := db.Query(`
+        SELECT name
+        FROM sqlite_master
+        WHERE type='table' AND name='users'
+    `)
+    if err != nil {
+        t.Fatalf("error querying database: %v", err)
+    }
+    defer rows.Close()
+
+    if !rows.Next() {
+        t.Error("users table not found in the database")
+    }
+
+    columns, err := db.Query("PRAGMA table_info(users)")
+    if err != nil {
+        t.Fatalf("error querying database: %v", err)
+    }
+    defer columns.Close()
+
+    emailFound := false
+    passwordFound := false
+    for columns.Next() {
+        var (
+            id         int
+            name       string
+            dataType   string
+            allowNull  int
+            defaultVal interface{}
+            primaryKey int
+        )
+        if err := columns.Scan(&id, &name, &dataType, &allowNull, &defaultVal, &primaryKey); err != nil {
+			t.Fatalf("error scanning columns: %v", err)
+		}
+		if name == "email" {
+			emailFound = true
+		}
+		if name == "password" {
+			passwordFound = true
+		}
+    }
+
+    if !emailFound {
+        t.Error("email column not found in the users table")
+    }
+
+    
+	if !passwordFound {
+		t.Error("password column not found in the users table")
+	}
+}
+
