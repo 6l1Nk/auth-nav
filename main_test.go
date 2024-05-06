840699ddbd598f3a71c10fa07934d6b82bc9d822
6l1Nk <nope@who.wat>
2024-05-06 00:02:01 -0500

    updated tests to use https

--- main_test.go
+++ main_test.go
@@ -1,116 +1,115 @@
 package main
 
 import (
-    "net/http"
-    "net/http/httptest"
-    "testing"
-    "strings"
 	"database/sql"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
 
-    _ "github.com/mattn/go-sqlite3"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func TestIndexHandler(t *testing.T) {
-    req, err := http.NewRequest("GET", "/", nil)
-    if err != nil {
-        t.Fatal(err)
-    }
+	req, err := http.NewRequest("GET", "https://127.0.0.1:8443/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-    res := httptest.NewRecorder()
-    handler := http.HandlerFunc(indexHandler)
+	res := httptest.NewRecorder()
+	handler := http.HandlerFunc(indexHandler)
 
-    handler.ServeHTTP(res, req)
+	handler.ServeHTTP(res, req)
 
-    if status := res.Code; status != http.StatusOK {
-        t.Errorf(
-            "handler returned wrong status code: got %v and expected %v",
-            status, 
-            http.StatusOK)
-    }
+	if status := res.Code; status != http.StatusOK {
+		t.Errorf(
+			"handler returned wrong status code: got %v and expected %v",
+			status,
+			http.StatusOK)
+	}
 
-    if !strings.Contains(res.Body.String(),"<nav>") {
-        t.Errorf("handler returned unexpected body: missing <nav>")
-    }
-    
-    loginButtonHTML := "<button id=\"login\">"
-    if !strings.Contains(res.Body.String(), loginButtonHTML) {
-        t.Errorf("handler returned unexpected body: missing %v", loginButtonHTML)
-    }
+	if !strings.Contains(res.Body.String(), "<nav>") {
+		t.Errorf("handler returned unexpected body: missing <nav>")
+	}
 
-    signUpButtonHTML := "<button id=\"signup\">"
-    if !strings.Contains(res.Body.String(), signUpButtonHTML) {
-        t.Errorf("handler returned unexpected body: missing %v", signUpButtonHTML)
-    }
+	loginButtonHTML := "<button id=\"login\">"
+	if !strings.Contains(res.Body.String(), loginButtonHTML) {
+		t.Errorf("handler returned unexpected body: missing %v", loginButtonHTML)
+	}
+
+	signUpButtonHTML := "<button id=\"signup\">"
+	if !strings.Contains(res.Body.String(), signUpButtonHTML) {
+		t.Errorf("handler returned unexpected body: missing %v", signUpButtonHTML)
+	}
 }
 
 func TestSignUpGet(t *testing.T) {
-    req, err := http.NewRequest("GET", "/sign-up", nil)
-    if err != nil {
-        t.Fatal(err)
-    }
+	req, err := http.NewRequest("GET", "https://127.0.0.1:8443/sign-up", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-    res := httptest.NewRecorder()
-    handler := http.HandlerFunc(signUpHandler)
+	res := httptest.NewRecorder()
+	handler := http.HandlerFunc(signUpHandler)
 
-    handler.ServeHTTP(res, req)
+	handler.ServeHTTP(res, req)
 
-    if status := res.Code; status != http.StatusOK {
-        t.Errorf(
-            "handler returned wrong status code: got %v and expected %v",
-            status,
-            http.StatusOK)
-    }
+	if status := res.Code; status != http.StatusOK {
+		t.Errorf(
+			"handler returned wrong status code: got %v and expected %v",
+			status,
+			http.StatusOK)
+	}
 
-    signUpFormHTML := "<form action=\"/signup\" method=\"post\" class=\"signup-form\">" 
+	signUpFormHTML := "<form action=\"/signup\" method=\"post\" class=\"signup-form\">"
 
-    if !strings.Contains(res.Body.String(),signUpFormHTML) {
-        t.Errorf("handler returned unexpected body: missing %v",
-        signUpFormHTML)
-    }
+	if !strings.Contains(res.Body.String(), signUpFormHTML) {
+		t.Errorf("handler returned unexpected body: missing %v",
+			signUpFormHTML)
+	}
 }
 
-
 func TestDatabaseSchema(t *testing.T) {
-    initDatabase()
+	initDatabase()
 
 	db, err := sql.Open("sqlite3", "users.db")
 	if err != nil {
 		t.Fatalf("error opening datavase %v", err)
 	}
 	defer db.Close()
-    
-    rows, err := db.Query(`
+
+	rows, err := db.Query(`
         SELECT name
         FROM sqlite_master
         WHERE type='table' AND name='users'
     `)
-    if err != nil {
-        t.Fatalf("error querying database: %v", err)
-    }
-    defer rows.Close()
+	if err != nil {
+		t.Fatalf("error querying database: %v", err)
+	}
+	defer rows.Close()
 
-    if !rows.Next() {
-        t.Error("users table not found in the database")
-    }
+	if !rows.Next() {
+		t.Error("users table not found in the database")
+	}
 
-    columns, err := db.Query("PRAGMA table_info(users)")
-    if err != nil {
-        t.Fatalf("error querying database: %v", err)
-    }
-    defer columns.Close()
+	columns, err := db.Query("PRAGMA table_info(users)")
+	if err != nil {
+		t.Fatalf("error querying database: %v", err)
+	}
+	defer columns.Close()
 
-    emailFound := false
-    passwordFound := false
-    for columns.Next() {
-        var (
-            id         int
-            name       string
-            dataType   string
-            allowNull  int
-            defaultVal interface{}
-            primaryKey int
-        )
-        if err := columns.Scan(&id, &name, &dataType, &allowNull, &defaultVal, &primaryKey); err != nil {
+	emailFound := false
+	passwordFound := false
+	for columns.Next() {
+		var (
+			id         int
+			name       string
+			dataType   string
+			allowNull  int
+			defaultVal interface{}
+			primaryKey int
+		)
+		if err := columns.Scan(&id, &name, &dataType, &allowNull, &defaultVal, &primaryKey); err != nil {
 			t.Fatalf("error scanning columns: %v", err)
 		}
 		if name == "email" {
@@ -119,15 +118,13 @@ func TestDatabaseSchema(t *testing.T) {
 		if name == "password" {
 			passwordFound = true
 		}
-    }
+	}
 
-    if !emailFound {
-        t.Error("email column not found in the users table")
-    }
+	if !emailFound {
+		t.Error("email column not found in the users table")
+	}
 
-    
 	if !passwordFound {
 		t.Error("password column not found in the users table")
 	}
 }
-
